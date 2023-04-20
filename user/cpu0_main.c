5b3ae62429fc21103bfa6d69811be1a12ab12aad
Syhen_XX <71960229+qq1940793522@users.noreply.github.com>
2023-04-20 19:59:50 +0800

    Update cpu0_main.c

--- user/cpu0_main.c
+++ user/cpu0_main.c
@@ -1,35 +1,35 @@
 /*********************************************************************************************************************
-* TC264 Opensourec Library ����TC264 ��Դ�⣩��һ�����ڹٷ� SDK �ӿڵĵ�������Դ��
-* Copyright (c) 2022 SEEKFREE ��ɿƼ�
+* TC264 Opensourec Library 即（TC264 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2022 SEEKFREE 逐飞科技
 *
-* ���ļ��� TC264 ��Դ���һ����
+* 本文件是 TC264 开源库的一部分
 *
-* TC264 ��Դ�� ���������
-* �����Ը���������������ᷢ���� GPL��GNU General Public License���� GNUͨ�ù�������֤��������
-* �� GPL �ĵ�3�棨�� GPL3.0������ѡ��ģ��κκ����İ汾�����·�����/���޸���
+* TC264 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
 *
-* ����Դ��ķ�����ϣ�����ܷ������ã�����δ�������κεı�֤
-* ����û�������������Ի��ʺ��ض���;�ı�֤
-* ����ϸ����μ� GPL
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
 *
-* ��Ӧ�����յ�����Դ���ͬʱ�յ�һ�� GPL �ĸ���
-* ���û�У������<https://www.gnu.org/licenses/>
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
 *
-* ����ע����
-* ����Դ��ʹ�� GPL3.0 ��Դ����֤Э�� ������������Ϊ���İ汾
-* ��������Ӣ�İ��� libraries/doc �ļ����µ� GPL3_permission_statement.txt �ļ���
-* ����֤������ libraries �ļ����� �����ļ����µ� LICENSE �ļ�
-* ��ӭ��λʹ�ò����������� ���޸�����ʱ���뱣����ɿƼ��İ�Ȩ����������������
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
 *
-* �ļ�����          cpu0_main
-* ��˾����          �ɶ���ɿƼ����޹�˾
-* �汾��Ϣ          �鿴 libraries/doc �ļ����� version �ļ� �汾˵��
-* ��������          ADS v1.8.0
-* ����ƽ̨          TC264D
-* ��������          https://seekfree.taobao.com/
+* 文件名称          cpu0_main
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          ADS v1.8.0
+* 适用平台          TC264D
+* 店铺链接          https://seekfree.taobao.com/
 *
-* �޸ļ�¼
-* ����              ����                ��ע
+* 修改记录
+* 日期              作者                备注
 * 2022-09-15       pudding            first version
 ********************************************************************************************************************/
 #include "zf_common_headfile.h"
@@ -40,35 +40,35 @@
 #include "Mahony_Icm20602.h"
 #include "PID.h"
 
-uint8 pit_state = 0;
+uint8 pit_state = 0;        //中断标志位
 uint8 count_1 = 0;
 uint8 count_2 = 0;
 uint8 count_3 = 0;
-float pitch_out = 0;        //pitch(��ת��)PWM���
-float roll_out = 0;         //roll(������)PWM���
-float yaw_out = 0;          //yaw(ƫ����)PWM���
+float pitch_out = 0;        //pitch(滚转角)PWM输出
+float roll_out = 0;         //roll(俯仰角)PWM输出
+float yaw_out = 0;          //yaw(偏航角)PWM输出
 
-// ���������#pragma section all restore���֮���ȫ�ֱ���������CPU0��RAM��
+// 将本语句与#pragma section all restore语句之间的全局变量都放在CPU0的RAM中
 
-// ���̵��뵽����֮��Ӧ��ѡ�й���Ȼ����refreshˢ��һ��֮���ٱ���
-// ����Ĭ������Ϊ�ر��Ż��������Լ��һ�����ѡ��properties->C/C++ Build->Setting
-// Ȼ�����Ҳ�Ĵ������ҵ�C/C++ Compiler->Optimization->Optimization level�������Ż��ȼ�
-// һ��Ĭ���½����Ĺ��̶���Ĭ�Ͽ�2���Ż�����˴��Ҳ��������Ϊ2���Ż�
+// 工程导入到软件之后，应该选中工程然后点击refresh刷新一下之后再编译
+// 工程默认设置为关闭优化，可以自己右击工程选择properties->C/C++ Build->Setting
+// 然后在右侧的窗口中找到C/C++ Compiler->Optimization->Optimization level处设置优化等级
+// 一般默认新建立的工程都会默认开2级优化，因此大家也可以设置为2级优化
 
-// ����TCϵ��Ĭ���ǲ�֧���ж�Ƕ�׵ģ�ϣ��֧���ж�Ƕ����Ҫ���ж���ʹ�� interrupt_global_enable(0); �������ж�Ƕ��
-// �򵥵�˵ʵ���Ͻ����жϺ�TCϵ�е�Ӳ���Զ������� interrupt_global_disable(); ���ܾ���Ӧ�κε��жϣ������Ҫ�����Լ��ֶ����� interrupt_global_enable(0); �������жϵ���Ӧ��
+// 对于TC系列默认是不支持中断嵌套的，希望支持中断嵌套需要在中断内使用 interrupt_global_enable(0); 来开启中断嵌套
+// 简单点说实际上进入中断后TC系列的硬件自动调用了 interrupt_global_disable(); 来拒绝响应任何的中断，因此需要我们自己手动调用 interrupt_global_enable(0); 来开启中断的响应。
 
-// �������ǿ�Դ����ֲ�ÿչ���
-// �������ǿ�Դ����ֲ�ÿչ���
-// �������ǿ�Դ����ֲ�ÿչ���
+// 本例程是开源库移植用空工程
+// 本例程是开源库移植用空工程
+// 本例程是开源库移植用空工程
 
-// **************************** �������� ****************************
+// **************************** 代码区域 ****************************
 int core0_main(void)
 {
-    clock_init();                   // ��ȡʱ��Ƶ��<��ر���>
-    debug_init();                   // ��ʼ��Ĭ�ϵ��Դ���
+    clock_init();                   // 获取时钟频率<务必保留>
+    debug_init();                   // 初始化默认调试串口
 
-    // �˴���д�û����� ���������ʼ�������
+    // 此处编写用户代码 例如外设初始化代码等
 
     motor_init();
     mahony_init();
@@ -76,11 +76,11 @@ int core0_main(void)
     pitch_pid_init();
     roll_pid_init();
 
-    // �˴���д�û����� ���������ʼ�������
-    cpu_wait_event_ready();         // �ȴ����к��ĳ�ʼ�����
+    // 此处编写用户代码 例如外设初始化代码等
+    cpu_wait_event_ready();         // 等待所有核心初始化完毕
     while (TRUE)
     {
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
 
         if(pit_state)
         {
@@ -146,13 +146,13 @@ int core0_main(void)
             moving_wheel_control(moving_wheel_dir, moving_wheel_pwm);
         }
 
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
     }
 }
 
 IFX_INTERRUPT(cc60_pit_ch0_isr, 0, CCU6_0_CH0_ISR_PRIORITY)
 {
-    interrupt_global_enable(0);                     // �����ж�Ƕ��
+    interrupt_global_enable(0);                     // 开启中断嵌套
     pit_clear_flag(CCU60_CH0);
 
     pit_state = 1;
@@ -160,4 +160,4 @@ IFX_INTERRUPT(cc60_pit_ch0_isr, 0, CCU6_0_CH0_ISR_PRIORITY)
 }
 
 #pragma section all restore
-// **************************** �������� ****************************
+// **************************** 代码区域 ****************************
