c797a3c49d47fc33c7c15c62b54cbc297d974b40
CJR11 <131381551+CJR11@users.noreply.github.com>
2023-04-23 20:22:18 +0800

    Update cpu1_main.c

--- user/cpu1_main.c
+++ user/cpu1_main.c
@@ -1,35 +1,35 @@
 /*********************************************************************************************************************
-* TC264 Opensourec Library ����TC264 ��Դ�⣩��һ�����ڹٷ� SDK �ӿڵĵ�������Դ��
-* Copyright (c) 2022 SEEKFREE ��ɿƼ�
+* TC264 Opensourec Library 即（TC264 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2022 SEEKFREE 逐飞科技
 *
-* ���ļ��� TC264 ��Դ���һ����
+* 本文件是 TC264 开源库的一部分
 *
-* TC264 ��Դ�� ���������
-* �����Ը���������������ᷢ���� GPL��GNU General Public License���� GNUͨ�ù�������֤��������
-* �� GPL �ĵ�3�棨�� GPL3.0������ѡ��ģ��κκ����İ汾�����·�����/���޸���
+* TC264 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
 *
-* ����Դ��ķ�����ϣ�����ܷ������ã�����δ�������κεı�֤
-* ����û�������������Ի��ʺ��ض���;�ı�֤
-* ����ϸ����μ� GPL
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
 *
-* ��Ӧ�����յ�����Դ���ͬʱ�յ�һ�� GPL �ĸ���
-* ���û�У������<https://www.gnu.org/licenses/>
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
 *
-* ����ע����
-* ����Դ��ʹ�� GPL3.0 ��Դ����֤Э�� ������������Ϊ���İ汾
-* ��������Ӣ�İ��� libraries/doc �ļ����µ� GPL3_permission_statement.txt �ļ���
-* ����֤������ libraries �ļ����� �����ļ����µ� LICENSE �ļ�
-* ��ӭ��λʹ�ò����������� ���޸�����ʱ���뱣����ɿƼ��İ�Ȩ����������������
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
 *
-* �ļ�����          cpu1_main
-* ��˾����          �ɶ���ɿƼ����޹�˾
-* �汾��Ϣ          �鿴 libraries/doc �ļ����� version �ļ� �汾˵��
-* ��������          ADS v1.8.0
-* ����ƽ̨          TC264D
-* ��������          https://seekfree.taobao.com/
+* 文件名称          cpu1_main
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          ADS v1.8.0
+* 适用平台          TC264D
+* 店铺链接          https://seekfree.taobao.com/
 *
-* �޸ļ�¼
-* ����              ����                ��ע
+* 修改记录
+* 日期              作者                备注
 * 2022-09-15       pudding            first version
 ********************************************************************************************************************/
 
@@ -41,34 +41,34 @@
 #include "Mahony_icm20602.h"
 #include "PID.h"
 
-// ���������#pragma section all restore���֮���ȫ�ֱ���������CPU1��RAM��
+// 将本语句与#pragma section all restore语句之间的全局变量都放在CPU1的RAM中
 
 
-// ���̵��뵽����֮��Ӧ��ѡ�й���Ȼ����refreshˢ��һ��֮���ٱ���
-// ����Ĭ������Ϊ�ر��Ż��������Լ��һ�����ѡ��properties->C/C++ Build->Setting
-// Ȼ�����Ҳ�Ĵ������ҵ�C/C++ Compiler->Optimization->Optimization level�������Ż��ȼ�
-// һ��Ĭ���½����Ĺ��̶���Ĭ�Ͽ�2���Ż�����˴��Ҳ��������Ϊ2���Ż�
+// 工程导入到软件之后，应该选中工程然后点击refresh刷新一下之后再编译
+// 工程默认设置为关闭优化，可以自己右击工程选择properties->C/C++ Build->Setting
+// 然后在右侧的窗口中找到C/C++ Compiler->Optimization->Optimization level处设置优化等级
+// 一般默认新建立的工程都会默认开2级优化，因此大家也可以设置为2级优化
 
-// ����TCϵ��Ĭ���ǲ�֧���ж�Ƕ�׵ģ�ϣ��֧���ж�Ƕ����Ҫ���ж���ʹ�� enableInterrupts(); �������ж�Ƕ��
-// �򵥵�˵ʵ���Ͻ����жϺ�TCϵ�е�Ӳ���Զ������� disableInterrupts(); ���ܾ���Ӧ�κε��жϣ������Ҫ�����Լ��ֶ����� enableInterrupts(); �������жϵ���Ӧ��
+// 对于TC系列默认是不支持中断嵌套的，希望支持中断嵌套需要在中断内使用 enableInterrupts(); 来开启中断嵌套
+// 简单点说实际上进入中断后TC系列的硬件自动调用了 disableInterrupts(); 来拒绝响应任何的中断，因此需要我们自己手动调用 enableInterrupts(); 来开启中断的响应。
 
 
-// **************************** �������� ****************************
+// **************************** 代码区域 ****************************
 void core1_main(void)
 {
-    disable_Watchdog();                     // �رտ��Ź�
-    interrupt_global_enable(0);             // ��ȫ���ж�
+    disable_Watchdog();                     // 关闭看门狗
+    interrupt_global_enable(0);             // 打开全局中断
 
-    // �˴���д�û����� ���������ʼ�������
+    // 此处编写用户代码 例如外设初始化代码等
 
     ips200_init(IPS200_TYPE_PARALLEL8);
     ips200_set_color(RGB565_WHITE, RGB565_BLACK);
 
-    // �˴���д�û����� ���������ʼ�������
-    cpu_wait_event_ready();                 // �ȴ����к��ĳ�ʼ�����
+    // 此处编写用户代码 例如外设初始化代码等
+    cpu_wait_event_ready();                 // 等待所有核心初始化完毕
     while (TRUE)
     {
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
 
         ips200_show_string(20, 320-16*12, "Pitch");
         ips200_show_float(10, 320-16*11, pitch, 2, 4);
@@ -105,6 +105,13 @@ void core1_main(void)
         ips200_show_string(15, 320-16*5, "Banl_Wheel_2_Encoder:");
         ips200_show_int(185, 320-16*5, banlance_wheel_2_now_encoder, 3);
 
+        ips200_show_string(15, 320-16*4, "Acc-X");
+        ips200_show_string(100, 320-16*4, "Acc-Y");
+        ips200_show_string(180, 320-16*4, "Acc-Z");
+        ips200_show_float(10, 320-16*3, icm_now_ax, 1, 4);
+        ips200_show_float(90, 320-16*3, icm_now_ay, 1, 4);
+        ips200_show_float(170, 320-16*3, icm_now_az, 1, 4);        
+        
         ips200_show_string(10, 320-16*2, "Gyro-X");
         ips200_show_string(95, 320-16*2, "Gyro-Y");
         ips200_show_string(175, 320-16*2, "Gyro-Z");
@@ -112,7 +119,7 @@ void core1_main(void)
         ips200_show_float(90, 320-16*1, icm_now_gy, 1, 4);
         ips200_show_float(170, 320-16*1, icm_now_gz, 1, 4);
 
-        // �˴���д��Ҫѭ��ִ�еĴ���
+        // 此处编写需要循环执行的代码
     }
 }
 #pragma section all restore
