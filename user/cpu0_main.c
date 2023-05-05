812cfc854aabf49c2f9669d910b590a88f771bc5
Syhen_XX <1940793522@qq.com>
2023-05-05 14:26:10 +0800

    Update cpu0_main.c

--- user/cpu0_main.c
+++ user/cpu0_main.c
@@ -96,7 +96,7 @@ IFX_INTERRUPT(cc60_pit_ch0_isr, 0, CCU6_0_CH0_ISR_PRIORITY)
 
 IFX_INTERRUPT(cc60_pit_ch1_isr, 0, CCU6_0_CH1_ISR_PRIORITY)
 {
-    interrupt_global_enable(0);                     // 开启中断嵌套
+    //interrupt_global_enable(0);                     // 关闭中断嵌套
     pit_clear_flag(CCU60_CH1);
     //车辆控制
 
